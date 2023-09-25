291c8475dcbc8232f7ecef57641d52e8c920f1fe
mandorakannu <mandorakannu@gmail.com>
2023-09-25 13:56:53 +0530

    Update: Status Bar

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
-import { ScrollView } from "react-native";
+import { ScrollView, StatusBar } from "react-native";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import { NativeRouter, Routes, Route } from "react-router-native";
 import UniquePlant from "./components/UniquePlant";
 import Cart from "./components/Cart";
 import Mandorakannu from "./components/Mandorakannu";
+import { colors } from "./libs/colors";
 
 function App(): JSX.Element {
   return (
     <>
       <NativeRouter>
         <ScrollView>
+          <StatusBar backgroundColor={colors.teal}/>
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/cart" element={<Cart />} />
